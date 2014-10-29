4b237b7c964b8f781bae655c2a8bdcae0d24c75c
cortneyPop <cortney@opticpop.com>
2014-10-29 17:05:08 -0500

    changed stuff don't remember

--- js/main.js
+++ js/main.js
@@ -6,19 +6,22 @@
 // });
 
 
-var showRightPush = document.getElementById( 'showRightPush' ),
-    menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-    body = document.body;
+// var showRightPush = document.getElementById( 'showRightPush' ),
+//     menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+//     body = document.body;
 
-showRightPush.onclick = function() {
-    classie.toggle( this, 'active' );
-    classie.toggle( body, 'cbp-spmenu-push-toleft' );
-    classie.toggle( menuRight, 'cbp-spmenu-open' );
-    disableOther( 'showRightPush' );
-};
+// showRightPush.onclick = function() {
+//     classie.toggle( this, 'active' );
+//     classie.toggle( body, 'cbp-spmenu-push-toleft' );
+//     classie.toggle( menuRight, 'cbp-spmenu-open' );
+//     disableOther( 'showRightPush' );
+// };
  
-function disableOther( button ) {
-    if( button !== 'showRightPush' ) {
-        classie.toggle( showRightPush, 'disabled' );
-    }
-}
+// function disableOther( button ) {
+//     if( button !== 'showRightPush' ) {
+//         classie.toggle( showRightPush, 'disabled' );
+//     }
+// }
+
+
+
